64d721aa4aec3abcbe8e805d03535391ee1ec29e
Antoine Boileau <apem.boileau@gmail.com>
2018-06-24 16:48:16 +0200

    Add address controller, a job launcher and connection to redis db

---
+++ server.js
@@ -0,0 +1,27 @@
+// As early as possible in your application, require and configure dotenv.
+// https://www.npmjs.com/package/dotenv#usage
+require('dotenv').config();
+
+server = require('./lib/server/server-instance');
+
+const init = async () => {
+
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: true,
+      logEvents: ['response', 'request'],
+    },
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
